ddee7810f490c07a18be3068c036cd23db807cf3
songrenzhao <songren.zhao@aol.com>
2018-12-10 22:23:28 -0500

    added source files

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './views/home/home.jsx';
+import login from './views/users/login';
+import Navbar from './components/Navbar';
+import Document from './components/Document';
+
+const About = () => <h2>About</h2>;
+// const Users = () => <h2>Users</h2>; // Added users page
+
+const AppRouter = () => (
+	<Router>
+		<div>
+			<Navbar />
+			<Route path="/" exact component={Home} />
+			<Route path="/about/" component={About} />
+			<Route path="/users/" component={login} />
+			<Route path="/document/" component={Document} />
+		</div>
+	</Router>
+);
+
+export default AppRouter;
